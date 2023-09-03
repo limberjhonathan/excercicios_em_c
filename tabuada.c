0278dff8b2e746f23f58683c98c268bcd26bbf3a
Limber Jhonathan <limberjhonathan@gmail.com>
2023-09-03 14:06:02 -0300

    Atualização da tabuada

--- tabuada.c
+++ tabuada.c
@@ -6,18 +6,18 @@ int main() {
 	setlocale(LC_ALL, "portuguese");
 	int tamanho;
 
-	printf("Digite o tamanho de sua array: ");
+	printf("Digite a quantidades de tabuadas: ");
 	scanf_s("%d", &tamanho);
 
 	int* myArray = (int*)malloc(tamanho * sizeof(int));
 
 	for (int i = 0; i < tamanho; i++) {
-		printf("Digite o %d° valor de sua array: ", i+1);
+		printf("Digite o %dï¿½ valor de sua tabuada: ", i+1);
 		scanf_s("%d", &myArray[i]);
 	}
 	
 	for (int v = 0; v < tamanho; v++) {
-		printf("Essa é a tabuada do %d\n", v);
+		printf("Tabuada do %d\n", v);
 		for (int i = 0; i <= 10; i++) {
 			printf("%d X %d = %d\n", myArray[v], i, myArray[v] * i);
 		}
